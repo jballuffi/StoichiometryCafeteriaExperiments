03b68f1c8ff647b3126161ae27140b2bbd725fa7
Juliana Balluffi-Fry <jballuffi@gmail.com>
2020-04-27 17:06:03 -0230

    update themes

--- R/07-Figures feeding.R
+++ R/07-Figures feeding.R
@@ -12,13 +12,14 @@ effsC<-readRDS("Input/effects_coat.rds")
 effsT<-readRDS("Input/effects_temp.rds")
 effsP<-readRDS("Input/effects_pref.rds")
 
+themeblank <- theme(axis.title = element_text(size=14),
+                axis.text.x = element_text(size=10),
+                legend.key = element_blank(),
+                panel.background = element_blank(),
+                panel.border = element_rect(colour = "black", fill=NA, size=1))
+
 
 #### FIGURE 4 ####
-theme4 <- theme(axis.title = element_text(size=14),
-              axis.text.x = element_text(size=10),
-              legend.key = element_blank(),
-              panel.background = element_blank(),
-              panel.border = element_rect(colour = "black", fill=NA, size=1))
 
 #if you want a path plot, use ID_Year, not Eartag.
 (Intake<-ggplot(data=DTtrials)+
@@ -26,7 +27,7 @@ theme4 <- theme(axis.title = element_text(size=14),
   geom_jitter(aes(y=IR, x=Trial), width=.25, size=3)+
   labs(y="Spruce intake rate (g/kg/day)", x=" ")+
   ggtitle("A")+
-  theme4)
+  themeblank)
 
 (Pref<-ggplot(data=DTtrials)+
   geom_hline(yintercept=0, size=1.2, color="grey40", linetype="dashed")+
@@ -34,7 +35,7 @@ theme4 <- theme(axis.title = element_text(size=14),
   geom_jitter(aes(y=Diff_IR, x=Trial), width=.25, size=3)+
   labs(y="Preference for high-quality spruce", x=" ")+
   ggtitle("B")+
-  theme4)
+  themeblank)
 
 (Weight<-ggplot(data=DTtrials)+
   geom_hline(yintercept=0, size=1.2, color="grey40", linetype="dashed")+
@@ -42,7 +43,7 @@ theme4 <- theme(axis.title = element_text(size=14),
   geom_jitter(aes(y=Mass_change, x=Trial), width=.25, size=3)+
   labs(y="Lost body mass (%)", x="Trial Number")+
   ggtitle("C")+
-  theme4)
+  themeblank)
 
 
 Fig4<-ggarrange(Intake, Pref, Weight,  ncol=1, nrow=3, label.x = 3)
@@ -51,14 +52,6 @@ ggsave(filename="Findings/Figure4.jpeg", Fig4, width = 5.5, height = 11.5, units
 
 
 #### Figure 5 ####
-theme5 <- theme(axis.title.y = element_text(size=14),
-                axis.title.x = element_text(size=12),
-                axis.text.x = element_text(size=10),
-                legend.key = element_blank(),
-                panel.background = element_blank(),
-                panel.grid.minor.y = element_line(color="grey"),
-                panel.grid.major.y = element_line(color="grey"),
-                panel.border = element_rect(colour = "black", fill=NA, size=1))
 
 #Boxplot showing the total trend
 (boxplot<-ggplot(data=DTpiles, aes(y=IR, x=Treatment))+
@@ -66,14 +59,14 @@ theme5 <- theme(axis.title.y = element_text(size=14),
    geom_jitter( width=.25, size=3)+
    labs(y="Spruce intake rate (g/kg/day)", x="Spruce quality")+
    ggtitle("A")+
-   theme5)
+   themeblank)
 
 #path plot that matches boxplot
 (pathplot<-ggplot(data=DTpiles)+
     geom_path(aes(y=IR, x=Treatment, group=sampleID), size=1)+
     labs(y=" ", x="Spruce quality")+
     ggtitle("B")+
-    theme5)
+    themeblank)
 
 Fig5<- ggarrange(boxplot, pathplot,  ncol=2, nrow=1, label.x = 3)
 ggsave(filename="Findings/Figure5.jpeg", Fig5, width = 7.3, height = 4.3, units = "in")
@@ -121,12 +114,8 @@ ggsave(filename="Findings/Figure6.jpeg", Fig6, width = 9, height = 4.5, units = 
     geom_abline(intercept = 0.06, slope = -0.0003809, size=1.2)+
     geom_vline(xintercept=0, size=1.2, color="grey40", linetype="dashed")+
     labs(x="Preference for high-quality spruce", y="Lost body mass(%)")+
-    theme(axis.title = element_text(size=14),
-          axis.text.x = element_text(size=10),
-          axis.text.y = element_text(size=10),
-          legend.key = element_blank(),
-          panel.background = element_blank(),
-          panel.border = element_rect(colour = "black", fill=NA, size=1)))
+    themeblank)
+
 ggsave(filename="Findings/Figure7.jpeg", Fig7, width = 6, height = 4, units = "in")
 
 
@@ -139,13 +128,7 @@ summary(lm(Diff_IR~IR, data=DTtrials))
 (Fig8<-ggplot(data=DTtrials)+
   geom_abline(intercept=1.13, slope=0.038, size=1.2)+
   geom_point(aes(y=Diff_IR, x=IR), size=4, colour="grey20")+
-  theme(axis.title.x = element_text(size=14),
-        axis.title.y = element_text(size=14),
-        axis.text.x = element_text(size=9),
-        axis.text.y = element_text(size=9),
-        legend.key = element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_rect(colour = "black", fill=NA, size=1)))
+  themeblank
 
 
 
