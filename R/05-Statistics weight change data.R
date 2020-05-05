934ee9accc193e6cbd70fbcbf96aa25f496c3290
Juliana Balluffi-Fry <jballuffi@gmail.com>
2020-05-05 12:11:28 -0230

    bring in output table cleaning code from feeding response script

--- R/05-Statistics weight change data.R
+++ R/05-Statistics weight change data.R
@@ -1,4 +1,4 @@
-libs<-c("data.table",'stargazer', 'AICcmodavg', 'ggplot2','RColorBrewer','lme4','pwr','MuMIn','nlme', 'ggeffects', 'arm', 'rsq')
+libs<-c("data.table",'stargazer', 'AICcmodavg', 'MuMIn', 'ggeffects', 'arm', 'rsq', 'dplyr')
 lapply(libs, require, character.only = TRUE)
 
 #import trial format
@@ -10,21 +10,13 @@ DTtrials<-DTtrials[!is.na(Mass_change)]
 #### Weight Change AIC ####
 
 Null <- lm(Mass_change ~ IR, data = DTtrials)
-
 Base <- lm(Mass_change ~ IR + Diff_IR, data = DTtrials)
-
 Temp <- lm(Mass_change ~ IR + Low_temp*Diff_IR, data = DTtrials)
-
 Coat <- lm(Mass_change ~ IR + White*Diff_IR, data = DTtrials)
-
 Energetic <- lm(Mass_change ~ IR + White*Diff_IR + Low_temp*Diff_IR, data = DTtrials)
-
 Nitrogen <- lm(Mass_change ~ IR + N_mean*Diff_IR, data = DTtrials)
-
 Phosphorus <- lm(Mass_change ~ IR + P_mean*Diff_IR, data = DTtrials)
-
 Nutrient <- lm(Mass_change ~ IR + N_mean*Diff_IR + P_mean*Diff_IR, data = DTtrials)
-
 Full <- lm(Mass_change ~ IR + White*Diff_IR + Low_temp*Diff_IR + N_mean*Diff_IR + P_mean*Diff_IR, data = DTtrials)
 
 #List and AIC
@@ -32,6 +24,9 @@ Mods<-list(Null, Base, Temp, Coat, Energetic, Nitrogen, Phosphorus, Nutrient, Fu
 Names<-c('Null', 'Base', 'Temp', 'Coat', 'Energetic', 'Nitrogen', 'Phosphorus', 'Nutrient', 'Full')
 AIC<-as.data.table(aictab(REML=F, cand.set = Mods, modnames = Names, sort = TRUE))
 AIC[,ModelLik:=NULL]
+AIC[,Cum.Wt:=NULL]
+#round whole table to 3 dec places
+AIC<-AIC %>% mutate_if(is.numeric, round, digits=3)
 
 
 
@@ -61,6 +56,59 @@ outputfun <- function(model) {
 OutAll<-lapply(Mods, outputfun)
 OutAll<-rbindlist(OutAll, fill = TRUE)
 OutAll$Model<-Names
+OutAll[,`(Intercept)`:=NULL]
+
+#function to swap out specific words in column names for new ones
+nameswap <- function(old, new, Data) {
+  older<-colnames(Data)[grep(old, colnames(Data))]
+  newer <- gsub(old, new, older)
+  setnames(Data, older, newer)
+}
+nameswap(old='Diff_IR', new='Pref', Data=OutAll)
+nameswap(old='White', new='Coat', Data=OutAll)
+nameswap(old='Low_temp', new='Temp', Data=OutAll)
+nameswap(old='N_mean', new='N', Data=OutAll)
+nameswap(old='P_mean', new='P', Data=OutAll)
+
+####Combine interaction outputs whoe column names were reversed
+
+#grabbinging column names with a ":" i.e. interaction outputs
+oldcols <- colnames(OutAll)[grepl(':', colnames(OutAll))]
+#re-order the column names
+newcols <- vapply(strsplit(oldcols, ':'), function(x) paste0(sort(x), collapse = ':'), 
+                  'potato')
+
+#function to merge two columns of the same interaction coef
+fix <- function(version1, version2, data) {
+  coal <- coalesce(data[[version1]], data[[version2]])
+  data[, (version1) := coal]
+  data[, (version2) := NULL]
+}
+
+#apply fix function to the grabbed columns
+lapply(seq.int(oldcols), function(i){
+  v1 <- newcols[[i]]
+  v2 <- oldcols[[i]]
+  if(v1 != v2){
+    fix(v1, v2, OutAll)
+  }
+})
+
+#swaping out ":" for "*"
+nameswap(old=':', new='*', Data=OutAll)
+#one last name change
+setnames(OutAll, "Pref*Temp", "Temp*Pref")
+
+#now reorder the cols
+setcolorder(OutAll, c("Model", 
+                      "IR", 
+                      "Pref", 
+                      "Temp", "Temp*Pref", 
+                      "Coat", "Coat*Pref",
+                      "N", "N*Pref",
+                      "P", "P*Pref",
+                      "rsq"))
+
 
 
 #saving the effects
