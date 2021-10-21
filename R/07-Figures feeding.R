72917bd4aa3851caf99d9c0782306d00e52891b5
Juliana Balluffi-Fry <jballuffi@gmail.com>
2021-10-21 16:56:18 -0600

    change theme and subtitle labels for journal

--- R/07-Figures feeding.R
+++ R/07-Figures feeding.R
@@ -12,18 +12,26 @@ effsC<-readRDS("Input/effects_coat.rds")
 effsT<-readRDS("Input/effects_temp.rds")
 effsP<-readRDS("Input/effects_pref.rds")
 
-themeblank <- theme(axis.title = element_text(size=14),
-                axis.text.x = element_text(size=10),
-                legend.key = element_blank(),
-                legend.position = "none",
-                panel.background = element_blank(),
-                panel.border = element_rect(colour = "black", fill=NA, size=1))
 
-themeblankguide <- theme(axis.title = element_text(size=14),
-                    axis.text.x = element_text(size=10),
+
+themeblank <- theme(axis.title = element_text(size=10),
+                    axis.text.x = element_text(size=8),
+                    axis.text.y = element_text(size=8),
                     legend.key = element_blank(),
                     panel.background = element_blank(),
-                    panel.border = element_rect(colour = "black", fill=NA, size=1))
+                    axis.line.y.left = element_line(color="black", size = .5),
+                    axis.line.x.bottom = element_line(color="black", size = .5),
+                    legend.position = "right",
+                    legend.direction = "vertical",
+                    legend.text = element_text(size=9),
+                    legend.title = element_text(size=11))
+
+themeblankguide <- theme(axis.title = element_text(size=10),
+                    axis.text.x = element_text(size=8),
+                    legend.key = element_blank(),
+                    panel.background = element_blank(),
+                    axis.line.y.left = element_line(color="black", size = .5),
+                    axis.line.x.bottom = element_line(color="black", size = .5))
 
 qualcols<-c("High" = "Forestgreen", "Low" = "Yellow3")
 
@@ -33,28 +41,25 @@ qualcols<-c("High" = "Forestgreen", "Low" = "Yellow3")
 (Intake<-ggplot(data=DTtrials)+
   geom_boxplot(position="dodge", aes(y=IR, x=Trial), outlier.shape = NA)+
   geom_jitter(aes(y=IR, x=Trial), width=.25, size=3)+
-  labs(y="Spruce intake rate (g/kg/day)", x=" ")+
-  ggtitle("A")+
+  labs(y="Spruce intake rate (g/kg/day)", x=" ", subtitle = "(a)")+
   themeblank)
 
 (Pref<-ggplot(data=DTtrials)+
   geom_hline(yintercept=0, size=1.2, color="grey40", linetype="dashed")+
   geom_boxplot(position="dodge", aes(y=Diff_IR, x=Trial), outlier.shape = NA)+
   geom_jitter(aes(y=Diff_IR, x=Trial), width=.25, size=3)+
-  labs(y="Preference for high-quality spruce", x=" ")+
-  ggtitle("B")+
+  labs(y="Preference for high-quality spruce", x=" ", subtitle = "(b)")+
   themeblank)
 
 (Weight<-ggplot(data=DTtrials)+
   geom_hline(yintercept=0, size=1.2, color="grey40", linetype="dashed")+
   geom_boxplot(position="dodge", aes(y=Mass_change, x=Trial), outlier.shape = NA)+
   geom_jitter(aes(y=Mass_change, x=Trial), width=.25, size=3)+
-  labs(y="Lost body mass (%)", x="Trial Number")+
-  ggtitle("C")+
+  labs(y="Lost body mass (%)", x="Trial Number", subtitle = "(c)")+
   themeblank)
 
 
-(Fig4<-ggarrange(Intake, Pref, Weight,  ncol=1, nrow=3, label.x = 3))
+(FigA6<-ggarrange(Intake, Pref, Weight,  ncol=1, nrow=3, label.x = 3))
 
 
 
@@ -65,21 +70,19 @@ qualcols<-c("High" = "Forestgreen", "Low" = "Yellow3")
    geom_boxplot(position="dodge", notch=FALSE)+ 
    geom_jitter(width=.25, size=2.5, colour="grey70")+
    scale_color_manual(values=qualcols)+
-   labs(y="Spruce intake rate (g/kg/day)", x="Spruce quality")+
-   ggtitle("A")+
+   labs(y="Spruce intake rate (g/kg/day)", x="Spruce quality", subtitle = "(a)")+
    themeblank)
 
 #path plot that matches boxplot
 (pathplot<-ggplot(data=DTpiles)+
     geom_path(aes(y=IR, x=Treatment, group=sampleID, colour=Eartag), size=1)+
     scale_x_discrete(expand = c(.2, .2))+
-    gghighlight(sampleID %in% list("A1680_10/27/2019", "A1667_11/24/2019"), max_highlight = 2, use_direct_label = NULL)+
-    labs(y=" ", x="Spruce quality")+
+    gghighlight(sampleID %in% list("A1680_10/27/2019", "A1667_11/24/2019"), max_highlight = 2)+
+    labs(y=" ", x="Spruce quality", subtitle = "(b)")+
     scale_color_manual(values=c("#1b9e77", "#d95f02"))+
-    ggtitle("B")+
-    themeblank)
+    themeblank+theme(legend.position = "none"))
 
-(Fig5<- ggarrange(boxplot, pathplot,  ncol=2, nrow=1, label.x = 3))
+(Fig1<- ggarrange(boxplot, pathplot,  ncol=2, nrow=1, label.x = 3))
 
 
 #### Figure 2 ####
@@ -91,7 +94,7 @@ qualcols<-c("High" = "Forestgreen", "Low" = "Yellow3")
     geom_ribbon(aes(x=x, ymin=conf.low, ymax=conf.high, group=group, fill=group),
               data=effsC, colour="grey80", alpha=.3 )+
     geom_line(aes(x=x, y=predicted, group=group), size = 1, color="grey 50", data=effsC)+
-    labs(x = "Individual coat insulation (% White)", y = "Spruce intake rate (g/kg/day)", title = "A")+
+    labs(x = "Individual coat insulation (% White)", y = "Spruce intake rate (g/kg/day)", subtitle = "(a)")+
     themeblankguide)
 
 #plot that shows intake as a function of temperature with spruce quality
@@ -101,15 +104,15 @@ qualcols<-c("High" = "Forestgreen", "Low" = "Yellow3")
     geom_ribbon(aes(x=x, ymin=conf.low, ymax=conf.high, group=group, fill=group),
               data=effsT, colour="grey80", alpha=.3 )+
     geom_line(aes(x=x, y=predicted, group=group), size = 1, color="grey 50", data=effsT)+
-    labs(x = "Minimum temperature (°C)", y = " ", title = "B")+
+    labs(x = "Minimum temperature (°C)", y = " ", subtitle = "(b)")+
     themeblankguide)
 
-(Fig6<- (Coat + Temp & scale_fill_manual(values=qualcols, name = "Spruce quality")) + plot_layout(guides = 'collect'))
+(Fig2<- (Coat + Temp & scale_fill_manual(values=qualcols, name = "Spruce quality")) + plot_layout(guides = 'collect'))
 
 
 ##### Figure 3 ####
 
-(Fig7<-ggplot(DTtrials)+
+(Fig3 <- ggplot(DTtrials)+
     geom_ribbon(aes(x=x, ymin=conf.low, ymax=conf.high, group=group),
                 data=effsP, colour="grey80", alpha=.3 )+
     geom_line(aes(x=x, y=predicted, group=group), size = 1, color="grey 50", data=effsP)+
@@ -122,9 +125,9 @@ qualcols<-c("High" = "Forestgreen", "Low" = "Yellow3")
 ##### Figure A9 ####
 
 #Additional figure of Total consumption vs. preference
-Prefmod<-lm(Diff_IR~IR, data=DTtrials)
+Prefmod <- lm(Diff_IR~IR, data=DTtrials)
 
-(FigA9<-ggplot(data=DTtrials)+
+(FigA9 <- ggplot(data=DTtrials)+
     geom_point(aes(y=Diff_IR, x=IR), size=3, colour="grey20")+
     labs(x="Total intake rate (g/kg/day)", y="Preference for high quality spruce")+
     themeblank)
@@ -132,12 +135,12 @@ Prefmod<-lm(Diff_IR~IR, data=DTtrials)
 
 
 #### Save figures ####
-ggsave(filename="Output/FigureA6.jpeg", Fig4, width = 5.5, height = 11.5, units = "in")
+ggsave(filename="Output/FigureA6.jpeg", FigA6, width = 5.5, height = 11.5, units = "in")
 
-ggsave(filename="Output/Figure1.jpeg", Fig5, width = 7.3, height = 4.3, units = "in")
+ggsave(filename="Output/Figure1.jpeg", Fig1, width = 7.3, height = 4.3, units = "in")
 
-ggsave(filename="Output/Figure2.jpeg", Fig6, width = 9, height = 4.5, units = "in")
+ggsave(filename="Output/Figure2.jpeg", Fig2, width = 9, height = 4.5, units = "in")
 
-ggsave(filename="Output/Figure3.jpeg", Fig7, width = 6, height = 4, units = "in")
+ggsave(filename="Output/Figure3.jpeg", Fig3, width = 6, height = 4, units = "in")
 
-ggsave(filename="Output/FigureA9.jpeg", FigA5, width = 6, height = 4, units = "in")
+ggsave(filename="Output/FigureA9.jpeg", FigA9, width = 6, height = 4, units = "in")
