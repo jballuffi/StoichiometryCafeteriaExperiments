b4931f8095c45e860beb4091a7f02cc3a59e9e07
Juliana Balluffi-Fry <jballuffi@gmail.com>
2021-10-21 17:07:54 -0600

    update panel labels

--- R/06-Figures plant.R
+++ R/06-Figures plant.R
@@ -55,7 +55,7 @@ thememap <- theme(axis.title = element_blank(),
 (Nmap<-ggplot(data=gridN) + 
     geom_raster(aes(x=x, y=y, fill=N))+
     scale_fill_gradient(high="darkgreen", low="yellow", name="N (%)              ")+
-    ggtitle("A) Nitrogen")+
+    labs(subtitle = "(a) Nitrogen")+
     geom_point(aes(POINT_X, POINT_Y, shape=Sampling), color="black", data = DTtraps, size=3)+
     scale_shape_manual(values=TrapShapes, name="Site Status")+
     thememap)
@@ -65,7 +65,7 @@ thememap <- theme(axis.title = element_blank(),
     scale_fill_gradient(high="darkgreen", low="yellow", name="P (%)              ")+
     geom_point(aes(POINT_X, POINT_Y, shape=Sampling), color="black", data = DTtraps, size=3)+
     scale_shape_manual(values=TrapShapes, name="Site Status")+
-    ggtitle("B) Phosphorus")+
+    labs(subtitle = "(b) Phosphorus")+
     thememap)
 
 (PSCmap<-ggplot(data=gridPSC) + 
@@ -73,11 +73,11 @@ thememap <- theme(axis.title = element_blank(),
     scale_fill_gradient(high="yellow", low="darkgreen", name="Terpene (mg/g)")+
     geom_point(aes(POINT_X, POINT_Y, shape=Sampling), color="black", data = DTtraps, size=3)+
     scale_shape_manual(values=TrapShapes, name="Site Status")+
-    ggtitle("C) Plant Secondary Compounds")+
+    labs(subtitle = "(c) Plant Secondary Compounds")+
     thememap)
 
 #make multi-panel
-(Fig1 <- Nmap / Pmap / PSCmap + plot_layout(guides = 'collect'))
+(FigA1 <- Nmap / Pmap / PSCmap + plot_layout(guides = 'collect'))
 
 
 #### FIGURE A2 ####
@@ -91,8 +91,7 @@ TrapShapes2<-c("Sampled"=16, "Offered"=8)
     geom_abline(intercept = 0.01354, slope = 0.1256)+
     geom_text(aes(.8, .2, label="y = 0.13x + 0.014"), size=3)+
     geom_text(aes(.8, .19, label="R2 = 0.43"), size=3)+
-    labs(y="% Phosphorus", x="% Nitrogen")+
-    ggtitle("A")+
+    labs(y="% Phosphorus", x="% Nitrogen", subtitle = "(a)")+
     themeblank)
 
 (NPSCscatter<-ggplot(data=DTtraps2)+
@@ -102,8 +101,7 @@ TrapShapes2<-c("Sampled"=16, "Offered"=8)
     geom_abline(intercept = 28.09, slope = -10.74)+
     geom_text(aes(1.27, 30, label="y = -10.74x + 28.09"), size=3)+
     geom_text(aes(1.27, 28, label="R2 = 0.17"), size=3)+
-    labs(y="Terpene content (mg/g)", x="% Nitrogen")+
-    ggtitle("B")+
+    labs(y="Terpene content (mg/g)", x="% Nitrogen", subtitle = "(b)")+
     themeblank)
 
 (PPSCscatter<-ggplot(data=DTtraps2)+
@@ -113,12 +111,11 @@ TrapShapes2<-c("Sampled"=16, "Offered"=8)
     geom_abline(intercept = 21.93, slope = -33.61)+
     geom_text(aes(.115, 31, label="y = -33.6x + 21.9"), size=3)+
     geom_text(aes(.115, 29, label="R2 = 0.059"), size=3)+
-    labs(y="Terpene content (mg/g)", x="% Phosphorus")+
-    ggtitle("C")+
+    labs(y="Terpene content (mg/g)", x="% Phosphorus", subtitle = "(c)")+
     themeblank)
 
 #make multi-panel
-(Fig2 <- (NPscatter / NPSCscatter / PPSCscatter) + plot_layout(guides = 'collect'))
+(FigA2 <- (NPscatter / NPSCscatter / PPSCscatter) + plot_layout(guides = 'collect'))
 
 
 
@@ -127,8 +124,7 @@ TrapShapes2<-c("Sampled"=16, "Offered"=8)
 (DBHN<-ggplot(data=DTtraps2)+
    geom_point(aes(y=N, x=AvgDBH), size = 3)+
    geom_text(aes(5, 1.35, label="p = 0.36"), size=3)+
-   labs(x=NULL, y="% Nitrogen")+
-   ggtitle("A")+
+   labs(x=NULL, y="% Nitrogen", subtitle = "(a)")+
    themeblank)
 
 (CanopyN<-ggplot(data=DTtraps2)+
@@ -136,15 +132,13 @@ TrapShapes2<-c("Sampled"=16, "Offered"=8)
     geom_abline(aes(intercept=.637, slope=.00336), size=1, colour="grey20")+
     geom_text(aes(25, 1.35, label="y = 0.00336x + 0.637"), size=3)+
     geom_text(aes(25, 1.25, label="t = 3.09, p < 0.01"), size=3)+
-    labs(x=NULL, y=NULL)+
-    ggtitle("B")+
+    labs(x=NULL, y=NULL, subtitle = "(b)")+
     themeblank)
 
 (DBHP<-ggplot(data=DTtraps2)+
     geom_point(aes(y=P, x=AvgDBH), size = 3)+
     geom_text(aes(5, .2, label="p = 0.69"), size=3)+
-    labs(x=NULL, y="% Phosphorus")+
-    ggtitle("C")+
+    labs(x=NULL, y="% Phosphorus", subtitle = "(c)")+
     themeblank)
 
 (CanopyP<-ggplot(data=DTtraps2)+
@@ -152,45 +146,40 @@ TrapShapes2<-c("Sampled"=16, "Offered"=8)
     geom_abline(aes(intercept=0.098, slope=0.00068), size=1, colour="grey20")+
     geom_text(aes(26, .2, label="y = 0.098x + 0.000682"), size=3)+
     geom_text(aes(26, .185, label="t = 3.041, p < 0.01"), size=3)+
-    labs(x=NULL, y=NULL)+
-    ggtitle("D")+
+    labs(x=NULL, y=NULL, subtitle = "(d)")+
     themeblank)
 
 (DBHC<-ggplot(data=DTtraps2)+
         geom_point(aes(y=TC, x=AvgDBH), size = 3)+
         geom_text(aes(12, 51, label="p = 0.80"), size=3)+
-        labs(x=NULL, y="% Carbon")+
-        ggtitle("E")+
+        labs(x=NULL, y="% Carbon", subtitle = "(e)")+
         themeblank)
 
 (CanopyC<-ggplot(data=DTtraps2)+
         geom_point(aes(y=TC, x=CanopyClosure), size = 3)+
         geom_text(aes(15, 51, label="p = 0.53"), size=5)+
-        labs(x=NULL, y=NULL)+
-        ggtitle("F")+
+        labs(x=NULL, y=NULL, subtitle = "(f)")+
         themeblank)
 
 (DBHPSC<-ggplot(data=DTtraps2)+
     geom_point(aes(y=PSC, x=AvgDBH), size = 3)+
     geom_text(aes(5, 30, label="p = 0.48"), size=3)+
-    labs(x="Mean DBH (cm)", y="PSC")+
-    ggtitle("G")+
+    labs(x="Mean DBH (cm)", y="PSC", subtitle = "(g)")+
     themeblank)
 
 (CanopyPSC<-ggplot(data=DTtraps2)+
     geom_point(aes(y=PSC, x=CanopyClosure), size = 3)+
     geom_text(aes(14, 30, label="p =0.11"), size=3)+
-    labs(x="Canopy Closure (%)", y=NULL)+
-    ggtitle("H")+
+    labs(x="Canopy Closure (%)", y=NULL, subtitle = "(h)")+
     themeblank)
 
 #make multi-panel
-Fig3<-ggarrange(DBHN, CanopyN, DBHP, CanopyP, DBHC, CanopyC, DBHPSC, CanopyPSC, ncol=2, nrow=4)
+FigA7<-ggarrange(DBHN, CanopyN, DBHP, CanopyP, DBHC, CanopyC, DBHPSC, CanopyPSC, ncol=2, nrow=4)
 
 
 
 
 #save all appendix figures
-ggsave(filename="Output/FigureA1.jpeg", Fig1, width = 4.75, height = 8.5, units = "in")
-ggsave(filename="Output/FigureA2.jpeg", Fig2, width = 6, height = 9.5, units = "in")
-ggsave(filename="Output/FigureA7.jpeg", Fig3, width = 9, height = 10, units = "in")
+ggsave(filename="Output/FigureA1.jpeg", FigA1, width = 4.75, height = 8.5, units = "in")
+ggsave(filename="Output/FigureA2.jpeg", FigA2, width = 6, height = 9.5, units = "in")
+ggsave(filename="Output/FigureA7.jpeg", FigA7, width = 9, height = 10, units = "in")
